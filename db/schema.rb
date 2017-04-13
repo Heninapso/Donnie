22000c9bd679f99fa7b55e3b837db60c2abaa415
Heninapso <heninapso@gmail.com>
2017-04-13 16:26:25 +0200

    index hr services added and working - to be styled

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413095858) do
+ActiveRecord::Schema.define(version: 20170413135412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,10 +19,11 @@ ActiveRecord::Schema.define(version: 20170413095858) do
     t.string   "title"
     t.text     "description"
     t.float    "price"
-    t.boolean  "bundle_option"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.boolean  "entry_service"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.string   "conditions"
+    t.boolean  "ondemand_service"
   end
 
   create_table "meetings", force: :cascade do |t|
