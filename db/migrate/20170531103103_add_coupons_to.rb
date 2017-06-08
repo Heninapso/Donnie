ffb2588cc3052e2fac2e8e68ee4cbaf9802b9b3d
Heninapso <heninapso@gmail.com>
2017-06-08 17:00:02 +0200

    test

--- db/migrate/20170531103103_add_coupons_to.rb
+++ db/migrate/20170531103103_add_coupons_to.rb
@@ -1,6 +1,7 @@
 class AddCouponsTo < ActiveRecord::Migration[5.0]
   def change
     add_reference :orders, :coupon, index: true
+    add_column :orders, :couponCode, :string
     add_foreign_key :orders, :coupons
   end
 end
