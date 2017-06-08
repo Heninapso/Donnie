5e7d3fe553b2c3456c11d60523fb9006a5696a9a
Heninapso <heninapso@gmail.com>
2017-06-08 17:05:08 +0200

    added migration for production environment

--- db/migrate/20170531103103_add_coupons_to.rb
+++ db/migrate/20170531103103_add_coupons_to.rb
@@ -1,7 +1,6 @@
 class AddCouponsTo < ActiveRecord::Migration[5.0]
   def change
     add_reference :orders, :coupon, index: true
-    add_column :orders, :couponCode, :string
     add_foreign_key :orders, :coupons
   end
 end
