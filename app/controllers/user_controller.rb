2574e74cd1e75f3e57b54ce771bf50ef8037acae
Heninapso <heninapso@gmail.com>
2017-06-12 15:28:14 +0200

    bug fixed on session_new

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,22 +1,22 @@
 class UserController < ApplicationController
 
-  def new
-    @user = User.new(user_params)
-  end
+  # def new
+  #   @user = User.new(user_params)
+  # end
 
-  def create
-    @user = User.find(user_params)
-    if @user.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
+  # def create
+  #   @user = User.find(user_params)
+  #   if @user.save
+  #     redirect_to root_path
+  #   else
+  #     render :new
+  #   end
+  # end
 
-  private
+  # private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password)
-  end
+  # def user_params
+  #   params.require(:user).permit(:first_name, :last_name, :email, :password)
+  # end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,16 @@ class User < ApplicationRecord
 
   has_many :meetings, dependent: :destroy
 
-  after_create :subscribe_to_newsletter
+  after_create :subscribe_to_newsletter, only: [:user_new?]
 
   private
 
   def subscribe_to_newsletter
     SubscribeToNewsletterService.new(self).call
+  end
+
+  def user_new?
+    User.exists?(self)
   end
 
 end
