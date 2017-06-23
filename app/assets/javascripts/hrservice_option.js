b97fa57ed1862bb4290623c8f8d4e874d6e2e3c4
Heninapso <heninapso@gmail.com>
2017-06-23 16:05:16 +0200

    test on js file for jquery update on price

--- app/assets/javascripts/hrservice_option.js
+++ app/assets/javascripts/hrservice_option.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-  $('#option-submit').on('click', function () {
+  $('#option-submit').on('change', function () {
     var option_selected = $('#option_selected').val();
     console_log(option_selected)
     var option_price = $(('#option_selected').data("price"));
     console_log(option_price);
-    $('#option_section').replaceWith("<p>Vous avez choisi : " +  option_selected + "</p>");
+    // $('#option_section').replaceWith("<p>Vous avez choisi : " +  option_selected + "</p>");
   });
 });
