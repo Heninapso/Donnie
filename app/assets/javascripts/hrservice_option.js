50dab3d8101ce44ed07a3b3ab992539ebe630e43
Heninapso <heninapso@gmail.com>
2017-06-27 17:45:39 +0100

    js implemented on option part

--- app/assets/javascripts/hrservice_option.js
+++ app/assets/javascripts/hrservice_option.js
@@ -4,12 +4,13 @@ $(document).ready(function () {
     console.log(option_selected)
     var option_price = $( "select option:selected" ).data("price");
     console.log(option_price);
+    $("#option_section").append("<p class=italic>" + option_price + " € </p>")
     var amount = parseInt($("#orderAmount").html());
     console.log(amount);
     var new_amount = option_price + amount;
     console.log(new_amount);
     $("#orderAmount").text(new_amount);
-    $(".amount").append('<p>test</p>');
+    $(".amount").append("<p class=italic>Option incluse : " + option_selected + " - "+ option_price+" € </p>");
     // $('#option_section').replaceWith("<p>Vous avez choisi : " +  option_selected + "</p>");
 
   });
